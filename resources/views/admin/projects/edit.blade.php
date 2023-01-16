475a57265582c1570612f196697444a3d75a1780
gioanbatan <giovannibassani@hotmail.com>
2023-01-16 23:33:39 +0100

    Add: edit with type

--- resources/views/admin/projects/edit.blade.php
+++ resources/views/admin/projects/edit.blade.php
@@ -26,6 +26,25 @@
                     </div>
 
                     <div class="mb-3">
+                        <label for="type" class="form-label">Tipologia</label>
+                        <select name="type_id" id="type"
+                            class="form-select @error('type_id')
+                        is-invalid
+                        @enderror">
+                            <option value="" selected>Nessuna tipologia selezionata</option>
+                            @foreach ($types as $type)
+                                <option value="{{ $type->id }}" @selected($project->type?->id == $type->id)>{{ $type->name }}
+                                </option>
+                            @endforeach
+                        </select>
+                        @error('type_id')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+
+                    <div class="mb-3">
                         <label for="cover_image" class="form-label">Immagine del progetto</label>
                         <input type="file" name="cover_image" id="cover_image"
                             class="form-control
